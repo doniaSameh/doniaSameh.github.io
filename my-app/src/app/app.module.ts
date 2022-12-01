37074b50fb3224ab4a0a57990c5dac0cd0b76576
Donia Sameh <donia2004436@miuegypt.edu.eg>
2022-12-01 23:39:55 +0200

    second component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SecondComponent
   ],
   imports: [
     BrowserModule,
